368869b9f4d5888529f5d8deaeba19c907bcf08b
Manel-Os <oulds.manel@gmail.com>
2023-05-29 17:27:58 -0400

    Added MyQueue and Stack_Queue to Windowed_max_range_Queue_Stack

---
+++ windowed_max_range_stack_queue/Stack_Queue.rb
@@ -0,0 +1,22 @@
+class StackQueue < MyStack
+    def initialize
+        @in_stack = MyStack.new
+        @out_stack = MyStack.new
+    end
+    def size
+        @in_stack.size + @out_stack.size
+    end
+    def empty?
+        @in_stack.empty? && @out_stack.empty?
+    end
+    def enqueue(ele)
+        @in_stack.push(ele)
+    end
+    def dequeue
+        queueify if @out_stack.empty?
+        @out_stack.pop
+    end
+    def queueify
+        @out_stack.push(@in_stack.pop) until @in_stack.empty?
+    end
+end
